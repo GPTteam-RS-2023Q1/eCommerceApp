56be60698d969017693f6e2dfe3ff07a82928466
intelligentRaji <sergdim1234@mail.ru>
2023-08-02 21:46:03 +0200

    EC-4 feat: add import-sorter

--- src/app/components/prostotak/prostotak.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-prostotak',
-  templateUrl: './prostotak.component.html',
-  styleUrls: ['./prostotak.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ProstotakComponent {}
