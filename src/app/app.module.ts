b78aebc30a57afa739236d218a8dfbb39ba9bf77
intelligentRaji <sergdim1234@mail.ru>
2023-08-03 14:11:12 +0200

    EC-4 fix: deteled useless code (app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
-import { ProstotakComponent } from './components/prostotak/prostotak.component';
 
 @NgModule({
-  declarations: [AppComponent, ProstotakComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
