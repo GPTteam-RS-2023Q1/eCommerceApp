36cae90f17b607a94c02cccb2c9f4459633e1fcf
Sobyt <kravczov.andrian@mail.ru>
2023-08-09 02:54:36 +0300

    EC-14 feat: add ngrx effect and setup ngrx folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
 
 import {
   TUI_SANITIZER,
@@ -12,16 +12,18 @@ import {
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 
 import { AppComponent } from './app.component';
+import { AppStoreModule } from './ngrx/store.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {}),
+    AppStoreModule,
     BrowserAnimationsModule,
     TuiRootModule,
     TuiDialogModule,
     TuiAlertModule,
+    HttpClientModule,
   ],
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
