0af52d84223e784515b9ca2c2e63ca7b532db045
Aliaksei <sladkiy.aleksey@gmail.com>
2023-08-09 21:44:04 +0200

    EC-34 feat: add authentication pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { AuthRoutingModule } from './auth-routing.module';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [RegistrationFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [RegistrationFormComponent, SignUpComponent, SignInComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
