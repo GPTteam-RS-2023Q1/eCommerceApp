3d2d6eb74801a1c9aacefc3b20b11366048ff201
intelligentRaji <sergdim1234@mail.ru>
2023-08-14 14:05:12 +0200

    EC-22 fix(sign-in): fix styles for sign-in form

--- src/app/auth/validators/email-validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-export function emailValidator(
-  control: AbstractControl<string>
-): ValidationErrors | null {
-  const { value } = control;
-  const emailPattern = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-  return emailPattern.test(value) ? null : { other: 'email is invalid' };
-}
