32b5c669c57571e18d820c1e398254eee6ff51ff
intelligentRaji <sergdim1234@mail.ru>
2023-08-16 02:00:56 +0200

    EC-22 feat(country data base): create country data base

--- src/app/consts/country-data.ts
+++ src/app/consts/country-data.ts
@@ -1,5 +1,14 @@
 import { MaskitoOptions } from '@maskito/core';
 
+export const defaultCountryData: CountryData = {
+  regExp: /.+/,
+  hint: '',
+  mask: {
+    mask: /.+/,
+  },
+  name: '',
+};
+
 export const COUNTRIES: Record<string, CountryData> = {
   Poland: {
     name: 'Poland',
@@ -7,6 +16,38 @@ export const COUNTRIES: Record<string, CountryData> = {
     hint: '12-345',
     mask: {
       mask: [/\d/, /\d/, '-', /\d/, /\d/, /\d/],
+    },
+  },
+  USA: {
+    name: 'USA',
+    regExp: /^\d{5}(-\d{4})?$/,
+    hint: '12345-6789',
+    mask: {
+      mask: [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
+    },
+  },
+  Canada: {
+    name: 'Canada',
+    regExp: /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/,
+    hint: 'K1A 0B1',
+    mask: {
+      mask: [/[A-Za-z]/, /\d/, /[A-Za-z]/, ' ', /\d/, /[A-Za-z]/, /\d/],
+    },
+  },
+  Belarus: {
+    name: 'Belarus',
+    regExp: /^\d{6}$/,
+    hint: '220000',
+    mask: {
+      mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/],
+    },
+  },
+  Ukraine: {
+    name: 'Ukraine',
+    regExp: /^\d{5}$/,
+    hint: '12345',
+    mask: {
+      mask: [/\d/, /\d/, /\d/, /\d/, /\d/],
     },
   },
 };
@@ -17,12 +58,3 @@ export interface CountryData {
   hint: string;
   mask: MaskitoOptions;
 }
-
-export const defaultCountryData: CountryData = {
-  regExp: /.+/,
-  hint: '',
-  mask: {
-    mask: /.+/,
-  },
-  name: '',
-};
