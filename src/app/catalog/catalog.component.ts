8a6bb6f9c60aa9faf5130d1de631bcf52008d362
Aliaksei <sladkiy.aleksey@gmail.com>
2023-09-04 18:58:25 +0200

    EC-60 feat: add menu for catalog

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,9 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { Observable, share } from 'rxjs';
+
+import { CategoryService } from '@app/core/services/category.service';
+
 @Component({
   selector: 'ec-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CatalogComponent {}
+export class CatalogComponent {
+  public isMenuOpen$: Observable<boolean>;
+
+  constructor(public categoryService: CategoryService) {
+    this.isMenuOpen$ = this.categoryService.menuSubject.asObservable().pipe(share());
+  }
+
+  public toggleMenu(): void {
+    const isOpen = this.categoryService.menuSubject.getValue();
+    this.categoryService.menuSubject.next(!isOpen);
+  }
+}
