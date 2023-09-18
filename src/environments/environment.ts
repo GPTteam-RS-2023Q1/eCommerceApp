c2dfe2c57e25e3d25ad7c6853570a6108602d3c6
intelligentRaji <sergdim1234@mail.ru>
2023-09-19 01:03:47 +0200

    EC-78 fix: change environments, add preconnect link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+/* export const environment = {
   production: true,
   CTP_PROJECT_KEY: 'ecommerce_gptteam',
   CTP_CLIENT_SECRET: 'AsD-hicDbtLa_kIwDlrb8OIG9-oxTuV5',
@@ -7,4 +7,13 @@ export const environment = {
   CTP_API_URL: 'https://api.europe-west1.gcp.commercetools.com',
   CTP_SCOPES:
     'view_categories:ecommerce_gptteam manage_my_orders:ecommerce_gptteam manage_customers:ecommerce_gptteam manage_my_quotes:ecommerce_gptteam create_anonymous_token:ecommerce_gptteam manage_my_business_units:ecommerce_gptteam view_published_products:ecommerce_gptteam manage_my_payments:ecommerce_gptteam view_products:ecommerce_gptteam manage_my_shopping_lists:ecommerce_gptteam manage_my_quote_requests:ecommerce_gptteam manage_my_profile:ecommerce_gptteam',
+}; */
+
+export const environment = {
+  CTP_PROJECT_KEY: 'ecommerce_gptteam',
+  CTP_CLIENT_SECRET: 'Z993NgtJSqM07NfE2tB79IovsEUJ3gFk',
+  CTP_CLIENT_ID: 'aGiPxeqE-JzyZdof4xJknosn',
+  CTP_AUTH_URL: 'https://auth.europe-west1.gcp.commercetools.com',
+  CTP_API_URL: 'https://api.europe-west1.gcp.commercetools.com',
+  CTP_SCOPES: 'manage_project:ecommerce_gptteam manage_api_clients:ecommerce_gptteam',
 };
